501512ee9508c614a2f23ee08e7914450d385f51
Paul Gudu <3348225661@qq.com>
2017-06-17 23:44:35 +0800

    completed question 8

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const loadItems = require('./loadItems.js');
+items = loadItems();
+module.exports = function main(input) {
+	numbers = input.split('');
+	var string = "";
+	for (var i = 0; i < numbers.length; i++) {
+		string += items[0][numbers[i]] ; 
+	}
+	string += "\n";
+	for (var i = 0; i < numbers.length; i++) {
+		string += items[1][numbers[i]]; 
+	}
+	string += "\n";
+	for (var i = 0; i < numbers.length; i++) {
+		string += items[2][numbers[i]]; 
+	}
+	string += "\n";
+    return string;
 };
